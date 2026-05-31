16654670694ed1dfce1eba6b6a89b648477ab125
Lester Johnson <webllrzjohnson@gmail.com>
2026-05-31 18:18:22 -0400

    fix: image not showing

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,8 @@ const nextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**.googleusercontent.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "drive.google.com" },
-      { protocol: "https", hostname: "**.google.com" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "placehold.co" },
-      { protocol: "https", hostname: "m.media-amazon.com" },
-      { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
 };
